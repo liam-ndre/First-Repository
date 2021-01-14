10a7bfbbdb4afd915cd567dce6f79170d31a7b9c
liam-ndre <liam.eguia2@mail.dcu.ie>
2021-01-14 20:17:35 +0000

    Adjusted formatting of name + age

--- pygame.py
+++ pygame.py
@@ -1,7 +1,6 @@
 print("Welcome to my first game!")
-name = input("What is your name?")
-age = input("What is your age")
+name = input("What is your name? ")
+age = input("What is your age? ")
 
-print("Hello", name, age, years of age)
+print("Hello", name,",", age, "years of age")
 health = 10
-
