43a4143b1b753a041b3a41e34c55245a08109874
liam-ndre <liam.eguia2@mail.dcu.ie>
2021-01-14 20:25:38 +0000

    Added age category

--- pygame.py
+++ pygame.py
@@ -1,6 +1,11 @@
 print("Welcome to my first game!")
 name = input("What is your name? ")
-age = input("What is your age? ")
+age = int(input("What is your age? "))
 
 print("Hello", name,",", age, "years of age")
 health = 10
+
+if age >= 18:
+    print("You are old enough to play this game!")
+else:
+    print("You are not old enough to play this game, sorry :(")
