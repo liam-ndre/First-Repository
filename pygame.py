0e756c5cdd4b3efceef90afdced69d8ce8e9cc66
liam-ndre <liam.eguia2@mail.dcu.ie>
2021-01-14 20:38:43 +0000

    Adjusted the age if statements

--- pygame.py
+++ pygame.py
@@ -7,5 +7,17 @@ health = 10
 
 if age >= 18:
     print("You are old enough to play this game!")
+
+    access_to_play = input("Do you want to play?")
+    if access_to_play == "yes" or "Yes":
+        print("Okay, let's play")
+
+elif age >= 14 and age <= 17:
+    print("You can play the game but with parental guidance :)")
+
+    access_to_play = input("Do you want to play?").lower()
+    if access_to_play == "yes":
+        print("Okay, let's play")
+
 else:
     print("You are not old enough to play this game, sorry :(")
