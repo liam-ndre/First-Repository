acc0587e791ed0bf0a0c7b4aa7737ca32a76730e
liam-ndre <liam.eguia2@mail.dcu.ie>
2021-01-18 17:28:17 +0000

    Completed Project

--- pygame.py
+++ pygame.py
@@ -22,63 +22,49 @@ if age >= 18:
             if equippable_sword == "yes":
                 print("You have now acquired a rust sword!")
 
+            elif equippable_sword == "no":
+                print("You have not acquired the rust sword and will not have the opportunity to do so for the rest of the game")
                 lake = input("You progress further and notice that there is a lake with stone path running across the middle. You also notice that you can go around but it will take longer. What path do you choose to take here? (Across/Around?) ").lower()
-                if lake == "across":
-                    print("You have made it across but have been bitten by a carnivorous fish. You have lost 5 health as a result")
-                    health -= 5
-                    print("Health = ", health, "HP")
-
+                
+            if lake == "across":
+                print("You have made it across but have been bitten by a carnivorous fish. You have lost 5 health as a result")
+                health -= 5
+                print("Health = ", health, "HP")
+            
+            elif lake == "around":
+                print("You went around safely")
+                    
                 new_area = input("After crossing the river you notice that there is a house about 100m north-west and a river 200m east. Which new area do you decide to go to? (House/River?)").lower
+                    
                 if new_area == "house":
                     print("You walk towards the wooden log house and see that the door is unlocked. You enter and notice that there is a man inside. He notices your health bar, walks over and decreases your health by")
                     health -= 5
                     print("Health = ", health, "HP")
                 
                 elif new_area == "river":
+                        
                     fishing_rod = input("You see a fishing rod, do you wish to pick it up?(Yes/No?) ").lower()
+                        
                     if fishing_rod == "yes":
                         print("You get a blister")
                         health -= 5
                         print("Health = ", health, "HP")
-
+                        
                 if health <= 0:
                     print("You have no more health, you slowly decend to a sweet slumber. :)")
-                    else:
-                        print("You survived and you win :)")
-                        
-            elif equippable_sword == "no":
-                print("You have not acquired the rust sword and will not have the opportunity to do so for the rest of the game")
-
+                            
+                else:
+                    print("You have survived and you win")
+           
             else:
                 print("The sword sees you as a threat for not complying with the rules and it kills you")
 
-        
         elif direction == "right":
             print("You die, F")
-        
+
         else:
             print("That was neither left or right. The path is confused and eats you up for not adhering to the rules")
-        
 
-elif age >= 14 and age <= 17:
-    print("You can play the game but with parental guidance :)")
-
-    access_to_play = input("Do you want to play? ").lower()
-    if access_to_play == "yes":
-        print("Okay, let's play")
-
-        direction = input("Left or Right? ").lower()
-        if direction == "left":
-            print("Okay let's go left")
-            print("Wow, you found a sword. Would you like to pick it up?")
-
-            equippable_sword = input("Yes or No? ").lower()
-        
-        elif direction == "right":
-            print("You die, F")
-        
-        else:
-            print("Oops, I couldn't lead you. Did you type either 'left' or 'right'?")
     else:
         print("Either you didn't type yes or you don't want to play :( . Either way I'll cya next time :)")
 
